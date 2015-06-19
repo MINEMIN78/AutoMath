ce53fcd293b84867a882c7b2985577727bf5b0e5
YUMA KOMORI <cubesugar78@gmail.com>
2015-06-20 02:47:56 +0900

    commit

--- AutoMath/ViewController.swift
+++ AutoMath/ViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var logoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        
+        //起動画面
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+            //imageview作成
+            self.logoImageView = UIImageView(frame: CGRectMake(0, 0, 1242, 2208))
+            //画面centerに
+            self.logoImageView.center = self.view.center
+            //logo設定
+            self.logoImageView.image = UIImage(named: "front.png")
+            //viewに追加
+            self.view.addSubview(self.logoImageView)
+        
+        
+        //色
         let colorKey = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1.0)
         let colorBg = UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1.0)
         //        let fontFamily: UIFont! = UIFont(name: "Hiragino Kaku Gothic ProN",size:10)?
@@ -22,9 +39,40 @@ class ViewController: UIViewController {
         UITabBar.appearance().barTintColor = colorBg
         
         
+        
+        
 
         
     }
+    
+    
+    //animation front view
+    override func viewDidAppear(animated: Bool) {
+        //少し縮小するアニメーション
+        UIView.animateWithDuration(0.3,
+            delay: 1.0,
+            options: UIViewAnimationOptions.CurveEaseOut,
+            animations: { () in
+                self.logoImageView.transform = CGAffineTransformMakeScale(0.9, 0.9)
+            }, completion: { (Bool) in
+                
+        })
+        
+        //拡大させて、消えるアニメーション
+        UIView.animateWithDuration(0.2,
+            delay: 1.3,
+            options: UIViewAnimationOptions.CurveEaseOut,
+            animations: { () in
+                self.logoImageView.transform = CGAffineTransformMakeScale(1.2, 1.2)
+                self.logoImageView.alpha = 0
+            }, completion: { (Bool) in
+                self.logoImageView.removeFromSuperview()
+        })        
+    }
+    
+    //animation end
+    
+    
     
     class MyFancyMenuViewController: UIViewController  {
         
